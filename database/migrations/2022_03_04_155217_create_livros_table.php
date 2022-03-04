b477dc7c17ecc7673dd3e9d9647b9c9d7e575586
guilhermeR20 <guilherme.rochalima17@gmail.com>
2022-03-04 14:41:49 -0300

    Laravel primeiro teste completo de tabelas

---
+++ database/migrations/2022_03_04_155217_create_livros_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('livros', function (Blueprint $table) {
+            $table->id();
+            $table->string('Autor'); // Nome do Autor
+            $table->date('dataProducao'); // Data de lançamento do livro e +
+            $table->string('titulo'); // Titulo de livro
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('livros');
+    }
+};
